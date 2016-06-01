9f996a32b517a1c6262c64c9cbdcd8a0e99c8dd3
Jackson Antonio do Prado Lima <jacksonpradolima@gmail.com>
2016-06-01 11:25:12 -0300

    Folder adjust

--- SUTs4MutationTesting/trytip/src/test/java/TryTipTest.java
+++ SUTs4MutationTesting/trytip/src/test/java/TryTipTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hg4hom;
+
 
 import org.junit.Test;
 import static org.junit.Assert.*;
